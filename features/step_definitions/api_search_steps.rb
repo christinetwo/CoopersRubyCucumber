c0dce1537cafbbb9ab0f45831b39e451f7d4adbb
christinetwo <christinetwo@gmail.com>
2022-01-02 11:48:24 -0600

    fixed key issue

--- features/step_definitions/api_search_steps.rb
+++ features/step_definitions/api_search_steps.rb
@@ -5,15 +5,14 @@ require 'json'
 When('I request a limited food search with the following parameters') do |request_table|
   #turn table to hash to search json,  all params are optional except query
   request_hash = request_table.hashes
-  rawHash = request_hash[0]
-  request_json = SearchEndpoint.new(rawHash["query"], rawHash["pageNumber"], rawHash["generalSearchInput"],
-                                    rawHash["numberOfResultsPerPage"], rawHash["pageSize"], rawHash["requireAllWords"])
+  raw_hash = request_hash[0]
+  request_json = SearchEndpoint.new(raw_hash["query"], raw_hash["pageNumber"], raw_hash["generalSearchInput"],
+                                    raw_hash["numberOfResultsPerPage"], raw_hash["pageSize"], raw_hash["requireAllWords"])
   request_body = request_json.to_json
   req = EndpointRequest.new("/foods/search", request_body)
   result = req.post_request
   @result_body = result.body
   @status_code = result.code
-
 end
 
 Then('I should see {int} result') do |expected_code|
@@ -23,6 +22,7 @@ end
 
 Then('I should see {string} returned in the {string} JSON foods path') do |expected_value, json_key|
   parsed_json = JSON.parse(@result_body)
+  #collect all values of json_key under foods path
   actual_list = parsed_json['foods'].collect { |item| item[json_key] }
   expect(actual_list).to include(expected_value)
 end
--- lib/endpoints/endpoint_request.rb
+++ lib/endpoints/endpoint_request.rb
@@ -1,24 +1,25 @@
 require 'fig_newton'
+require_relative '../../.api_key.rb'
 
 class EndpointRequest
 
   attr_accessor :url_part,:request_body
 
-  def initialize (url_part, request_body)
+  def initialize (url_part, request_body=nil)
     @url_part = url_part
     @request_body = request_body
+    endpoint_url_start = FigNewton.apiurl
+    endpoint_url = endpoint_url_start + @url_part
+    @uri = URI(endpoint_url)
+    @uri_api_key = "?api_key=#{$api_key}"
+    @https = Net::HTTP.new(@uri.host, @uri.port)
+    @https.use_ssl = true
   end
 
   def post_request
-    endpoint_url_start = FigNewton.apiurl
-    endpoint_url = endpoint_url_start + @url_part
-    uri = URI(endpoint_url)
-    uri_api_key = "?api_key=" + FigNewton.apaikey
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = true
-    req = Net::HTTP::Post.new(uri.path + uri_api_key, 'Content-Type' => 'application/json')
+    req = Net::HTTP::Post.new(@uri.path + @uri_api_key, 'Content-Type' => 'application/json')
     req.body = request_body
-    result = https.request(req)
+    result = @https.request(req)
   rescue => e
     puts "failed #{e}"
 
